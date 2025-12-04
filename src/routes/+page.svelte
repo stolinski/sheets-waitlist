d402755d60577da5dc928b2a28d12dd55b1871be
Scott Tolinski <scott.tolinski@gmail.com>
2025-12-03 19:12:25 -0700

    adds favicons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,11 @@
 	<meta property="og:image:secure_url" content="/og.png" />
 	<meta property="twitter:image" content="/og.png" />
 	<meta property="twitter:card" content="summary_large_image" />
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
 </svelte:head>
 
 <header>
