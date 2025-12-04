c8e1de64d9074afe9dd71813c45213aef41d58a2
Scott Tolinski <scott.tolinski@gmail.com>
2025-12-03 19:24:46 -0700

    fixes favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<meta property="og:type" content="website" />
+
+	<meta property="og:title" content="v_framer - Record Sources & Screens" />
+	<meta property="twitter:title" content="v_framer - Record Sources & Screens" />
+
+	<meta property="og:description" content="v_framer - Record Sources & Screens" />
+	<meta name="description" content="v_framer - Record Sources & Screens" />
+
+	<meta property="og:image" content="/og.png" />
+	<meta property="og:image:secure_url" content="/og.png" />
+	<meta property="twitter:image" content="/og.png" />
+	<meta property="twitter:card" content="summary_large_image" />
+
+	<link rel="icon" href="/favicon.ico" sizes="any" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+	<script defer data-domain="vframer.video" src="https://analytics.tolin.ski/js/script.js"></script>
 </svelte:head>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,27 +5,6 @@
 	let submitted = $state(false);
 </script>
 
-<svelte:head>
-	<meta property="og:type" content="website" />
-
-	<meta property="og:title" content="v_framer - Record Sources & Screens" />
-	<meta property="twitter:title" content="v_framer - Record Sources & Screens" />
-
-	<meta property="og:description" content="v_framer - Record Sources & Screens" />
-	<meta name="description" content="v_framer - Record Sources & Screens" />
-
-	<meta property="og:image" content="/og.png" />
-	<meta property="og:image:secure_url" content="/og.png" />
-	<meta property="twitter:image" content="/og.png" />
-	<meta property="twitter:card" content="summary_large_image" />
-
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-	<link rel="manifest" href="/site.webmanifest" />
-	<script defer data-domain="vframer.video" src="https://analytics.tolin.ski/js/script.js"></script>
-</svelte:head>
-
 <header>
 	<h1>v_framer</h1>
 	<p>Record all of your screens & video sources. No proprietary wrappers or bloat.</p>
