eb245c1e861b7b9a8f0451654bb488da36022359
Scott Tolinski <scott.tolinski@gmail.com>
2025-12-03 19:20:07 -0700

    adds plausible

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,6 +23,7 @@
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 	<link rel="manifest" href="/site.webmanifest" />
+	<script defer data-domain="vframer.video" src="https://analytics.tolin.ski/js/script.js"></script>
 </svelte:head>
 
 <header>
